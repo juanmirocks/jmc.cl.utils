a5694e3384fa74e55d4040a2f3067990cf62794e
Juan Miguel Cejuela <juanmi@jmcejuela.com>
2013-10-15 17:39:56 +0200

    implement range

--- src/sequences.lisp
+++ src/sequences.lisp
@@ -70,3 +70,7 @@
 (defun sequence->list (sequence)
   "Convert the sequence into a list"
   (concatenate 'list sequence))
+
+(defun range (max &key (min 0) (step 1))
+  (loop for n from min below max by step
+     collect n))

2fad51d01964c469bb63c40e29d08ca4b673cb58
Juan Miguel Cejuela <juanmi@jmcejuela.com>
2013-10-15 17:39:35 +0200

    export copy-array, copied from alexandria library

--- src/arrays.lisp
+++ src/arrays.lisp
@@ -243,9 +243,26 @@
 
 
 (defun copy-matrix (matrix)
-  "Copy a matrix"
+  "DEPRECATED, use `copy-array` -- Copy a matrix"
   (let* ((dims (array-dimensions matrix)) (dimx (car dims)) (dimy (cadr dims))
          (out (make-array dims :element-type (array-element-type matrix))))
     (dotimes (i dimx out)
       (dotimes (j dimy)
         (setf (aref out i j) (aref matrix i j))))))
+
+(defun copy-array (array &key (element-type (array-element-type array))
+                              (fill-pointer (and (array-has-fill-pointer-p array)
+                                                 (fill-pointer array)))
+                              (adjustable (adjustable-array-p array)))
+  "Returns an undisplaced copy of ARRAY, with same fill-pointer and
+adjustability (if any) as the original, unless overridden by the keyword
+arguments."
+ (let* ((dimensions (array-dimensions array))
+        (new-array (make-array dimensions
+                               :element-type element-type
+                               :adjustable adjustable
+                               :fill-pointer fill-pointer)))
+   (dotimes (i (array-total-size array))
+     (setf (row-major-aref new-array i)
+           (row-major-aref array i)))
+   new-array))
